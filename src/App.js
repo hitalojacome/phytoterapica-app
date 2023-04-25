503a417d348a57ae682abea7f39009cc6e516046
hitalojacome <106604805+hitalojacome@users.noreply.github.com>
2023-04-25 12:28:55 -0300

    adicionando componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './Components/Header';
+import Menu from './Components/Menu'
+import Introducao from './Components/Introducao'
+import Filtro from './Components/Filtro'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Menu />
+      <Introducao />
+      <Filtro />
     </div>
   );
 }
